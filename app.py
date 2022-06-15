8021be17a065291d3763f6a50ca147dab3854a5b
Obeta Ifeanyichukwu Malachy <angeloobeta@hotmail.com>
2022-06-15 15:55:46 +0100

    Add Ajax for handling client and server side request using the javascript fetch

--- app.py
+++ app.py
@@ -1,15 +1,61 @@
-from flask import Flask, render_template, request, redirect, url_for
-from flask_moment import Moment
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 import config
-from model import Person, db_setup
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import app
 
 app = Flask(__name__)
-moment = Moment(app)
-db = db_setup(app)
+# moment = Moment(app)
+# db = db_setup(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = config.SQLALCHEMY_DATABASE_URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # Enable debug mode.
 app.config['DEBUG'] = True
+
+db = SQLAlchemy(app)
+Migrate(app, db)
+
+
+# def db_setup(app):
+#     app.config.from_object('config')
+#     db.app = app
+#     db.init_app(app)
+#     migration = Migrate(app, db)
+#     return db
+
+
+class Person(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+    description = db.Column(db.String(), nullable=False)
+    completed = db.Column(db.Boolean, nullable=True)
+
+    def __int__(self, id, name, description, completed):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.completed = completed
+
+    def __repr__(self):
+        return f' {self.id} {self.name}'
+
+
+class Todo(db.Model):
+    __tablename__ = 'todos'
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(), nullable=False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
+
+    def __int__(self, id, description, completed):
+        self.id = id
+        self.description = description
+        self.completed = completed
+
+    def __repr__(self):
+        return f'<Todo {self.id} {self.description}>'
+
+
+db.create_all()
 
 
 # @app.route('/todos/create', methods=['POST'])
@@ -33,19 +79,27 @@ app.config['DEBUG'] = True
 #         else:
 #             return jsonify(body)
 
+@app.route('/todos/create', methods=['POST'])
+def create_todo():
+    # name = request.form.get('name', '')
+    name = request.get_json['name']
+    # description = request.form.get('description', '')
+    description = request.get_json['description']
+    person = Person(description=description, name=name)
+    db.session.add(person)
+    db.session.commit()
+    return jsonify({
+        'description': person.description,
+        'name': person.name,
+        'id': person.id
+    })
+    # return render_template('create.html', data=Person.query.all())
+    # return redirect(url_for('index'))
+
+
 @app.route('/')
 def index():
     return render_template('index.html', data=Person.query.all())
-
-
-# @app.route('/todo/create', methods=['POST'])
-# def create_todo():
-#     name = request.form.get('name', '')
-#     description = request.form.get('description', '')
-#     person = Person(description=description, name=name)
-#     db.session.add(person)
-#     db.session.commit()
-#     return redirect(url_for('edit.html', data=Person.query.all()))
 
 
 # db.create_all()
